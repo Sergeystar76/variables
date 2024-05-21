e76c391bd0972a1dfa58a9871fca53ee766bc199
Sergeystar76 <kertex@yandex.ru>
2024-05-21 14:35:47 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,12 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = list(students)
+students.sort()
+grades_0 = (sum(grades[0]) / len(grades[0]))
+grades_1 = (sum(grades[1]) / len(grades[1]))
+grades_2 = (sum(grades[2]) / len(grades[2]))
+grades_3 = (sum(grades[3]) / len(grades[3]))
+grades_4 = (sum(grades[4]) / len(grades[4]))
+students_new = {students[0]: grades_0, students[1]: grades_1, students[2]: grades_2, students[3]: grades_3,
+                students[4]: grades_4}
+print(students_new)
